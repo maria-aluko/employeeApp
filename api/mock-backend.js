476464eec4a21a8d0f06826144e3d7787bf86db7
Maria Aluko <maria.alukoo@gmail.com>
2025-01-07 17:27:30 +0200

    fix api mock backend

--- api/mock-backend.js
+++ api/mock-backend.js
@@ -1,13 +1,15 @@
-const jsonServer = require('json-server');
-const path = require('path');
+import jsonServer from 'json-server';
+import path from 'path';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const dbPath = path.join(process.cwd(), 'db.json');
+
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
+export default (req, res) => {
   server(req, res);
 };
