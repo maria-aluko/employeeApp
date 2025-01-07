091d19e6cf24470697f328f27f6e8369a2f0f754
Maria Aluko <35866661+maria-aluko@users.noreply.github.com>
2025-01-07 16:39:51 +0200

    Update EmployeeList.jsx

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -7,7 +7,7 @@ const EmployeeList = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://hr-app-maria-aluko.netlify.app/employees")
+    fetch("http://localhost:3001/employees")
     .then((response) => response.json())
     .then((data) => {
       setPersons(data);
