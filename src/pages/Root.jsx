603ed081677cc1dad254133c19fa82517832dd7c
Maria Aluko <maria.alukoo@gmail.com>
2025-01-07 14:03:37 +0200

    bug fix

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 
 const Root = () => {
   return (
