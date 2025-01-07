c2489133b7f0d49cc10f63c1a08dc829617b5783
Maria Aluko <maria.alukoo@gmail.com>
2025-01-07 12:51:07 +0200

    update Log in page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import Button from "../components/Button/Button";
+
 const Login = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const toggleLogin = () => {
+    setIsLoggedIn(prevState => !prevState);
+  };
+
   return (
-    <div>Login component</div>
+    <div>
+      <h2>{isLoggedIn ? 'You are logged in' : 'Please log in to access the Employee Database'}</h2>
+      <Button
+        onClick={toggleLogin}
+        text={isLoggedIn ? 'Log Out' : 'Log In'}
+      >
+      </Button>
+
+    </div>
   )
 }
 
