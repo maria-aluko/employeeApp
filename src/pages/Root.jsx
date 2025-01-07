5e332655f1ff7c16da76576ce72bb27a0897be45
Maria Aluko <maria.alukoo@gmail.com>
2025-01-07 14:46:24 +0200

    typo

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
-const Root = () => {
+function Root () {
   return (
     <div>
       <Header />
